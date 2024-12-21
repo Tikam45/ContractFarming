3be1d45f394c60144b0dd73c7b50d45a62751542
Tikam45 <138325804+Tikam45@users.noreply.github.com>
2024-12-21 17:02:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,7 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cors(
     {
-        origin: ["https://contract-farming-application.vercel.app/"],
-        methods: ["POST" , "GET", "PUT", "DELETE"],
-        credentials: true,
+        origin: "https://contract-farming-application.vercel.app/"
     }
 ));
 
